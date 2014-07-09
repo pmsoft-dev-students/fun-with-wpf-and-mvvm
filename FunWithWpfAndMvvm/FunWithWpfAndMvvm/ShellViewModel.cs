e8aa75f2416d9214d26a714f9e571028adaceb35
valmaev <almaev.vladimir@gmail.com>
2014-07-09 15:54:29 +0400

    Added ShellViewModel

---
+++ FunWithWpfAndMvvm/FunWithWpfAndMvvm/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace FunWithWpfAndMvvm
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            OpenChildViewCommand = new RelayCommand(OpenChildView);
+        }
+
+        public ICommand OpenChildViewCommand { get; private set; }
+
+        public void OpenChildView() {}
+    }
+}
